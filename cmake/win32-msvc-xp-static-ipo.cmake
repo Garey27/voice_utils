da9d387e9c9331213d72c1503d3baffd2db3bba8
Garey <ak.garey@gmail.com>
2021-05-18 00:10:22 +0500

    Update win32-msvc-xp-static-ipo.cmake

--- cmake/win32-msvc-xp-static-ipo.cmake
+++ cmake/win32-msvc-xp-static-ipo.cmake
@@ -1,11 +1,11 @@
 set(CMAKE_SYSTEM_VERSION 7.0)
 
-if(NOT CMAKE_GENERATOR STREQUAL "Visual Studio 15 2017")
-  message(FATAL_ERROR "Visual Studio 15 2017 generator is required (got ${CMAKE_GENERATOR})")
+if(NOT CMAKE_GENERATOR STREQUAL "Visual Studio 16 2019")
+  message(FATAL_ERROR "Visual Studio 15 2019 generator is required (got ${CMAKE_GENERATOR})")
 endif()
 
 set(CMAKE_GENERATOR_PLATFORM Win32)
-set(CMAKE_GENERATOR_TOOLSET v141_xp)
+set(CMAKE_GENERATOR_TOOLSET v142)
 
 if(CMAKE_VERSION VERSION_LESS "3.15")
   message(FATAL_ERROR "Minimum CMake 3.15 is required (got ${CMAKE_VERSION})")
